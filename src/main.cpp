01ee3fda0eb1e1a568b08d8952055a1c20c0df4f
ibm3650 <ibm3650@example.com>
2024-10-09 02:01:51 +0300

    Webserver base

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,21 @@
 #include "async_wait.h"
 #include <Wire.h>
 
-#define DS1307_ADDR 0x68
+#define EEPROM_ADDR  0x50
+#define PAGE_SIZE   32
+#define RTC_ADDR 0x68
 #define MAX_WIFI_TRIES  5
 #define SSID "MikroTik-B971FF"
 #define PASSWORD "pussydestroyer228"
+
+
+struct entry_t {
+    uint16_t start;
+    uint16_t end;
+    uint16_t transition_time;
+} __attribute__((packed));
+
+
 
 static WiFiEventHandler stationConnectedHandler;
 static WiFiEventHandler disconnectedEventHandler;
@@ -17,7 +28,7 @@ static pt web_server_context;
 static uint8_t tries_ctr = 0;
 
 
-void logMessage(String message) {
+void logMessage(const String& message) {
     Serial.println(message);  // Выводим сообщение в последовательный монитор
 }
 
@@ -30,14 +41,14 @@ void logRequest() {
 
     // Вывод параметров запроса, если есть
     if (server.args() > 0) {
-        for (uint8_t i = 0; i < server.args(); i++) {
+        for (int i = 0; i < server.args(); i++) {
             logMessage("Параметр: " + server.argName(i) + " = " + server.arg(i));
         }
     }
 
     // Вывод заголовков запроса, если есть
     if (server.headers() > 0) {
-        for (uint8_t i = 0; i < server.headers(); i++) {
+        for (int i = 0; i < server.headers(); i++) {
             logMessage("Заголовок: " + server.headerName(i) + " = " + server.header(i));
         }
     }
@@ -47,12 +58,12 @@ void logRequest() {
 
 
 
-void wifi_disconnect_cb(const WiFiEventStationModeDisconnected& event){
+void wifi_disconnect_cb(const WiFiEventStationModeDisconnected& /*event*/){
     PT_SCHEDULE_RESUME(&nm_context);
     ++tries_ctr;
 }
 
-void wifi_connected_cb(const WiFiEventStationModeConnected& event){
+void wifi_connected_cb(const WiFiEventStationModeConnected& /*event*/){
     tries_ctr = 0;
 }
 
@@ -65,7 +76,7 @@ byte bcdToDec(byte val) {
 }
 
 void setTime(byte hour, byte minute, byte second, byte day, byte month, byte year) {
-    Wire.beginTransmission(DS1307_ADDR);
+    Wire.beginTransmission(RTC_ADDR);
     Wire.write(0);  // Устанавливаем указатель на 0 регистр
     Wire.write(decToBcd(second));  // Секунды
     Wire.write(decToBcd(minute));  // Минуты
@@ -77,21 +88,45 @@ void setTime(byte hour, byte minute, byte second, byte day, byte month, byte yea
     Wire.endTransmission();
 }
 void setup() {
-    pinMode(D5, OUTPUT);
-    Serial.begin(115200);
-    Wire.begin();
-    setTime(12, 0, 0, 4, 10, 2024);
     PT_INIT(&nm_context);
     PT_INIT(&web_server_context);
+    Serial.begin(115200);
+    Wire.begin();
+    SPIFFS.begin();
     stationConnectedHandler = WiFi.onStationModeConnected(wifi_connected_cb);
     disconnectedEventHandler = WiFi.onStationModeDisconnected(wifi_disconnect_cb);
-    if(!SPIFFS.begin()){ //Инициализируем SPIFFS
-        Serial.println("An Error has occurred while mounting SPIFFS");
-        return;
-    }
-    server.on("/addRecord", []() {
-        logRequest();  // Логгируем данные запроса
-        Serial.println(server.arg("plain"));
+//    /pinMode(D5, OUTPUT);
+    //setTime(12, 0, 0, 4, 10, 2024);
+
+    //TODO: оптимизация парсинга json
+    //TODO: верификация получнного json для корректного ответа
+    server.on("/add_record", []() {
+        logRequest();
+
+        char values[4][8];
+        const auto payload = server.arg("plain");
+        size_t pos = 0;
+        size_t pos_old = 1;
+        size_t ctr = 0;
+        while((pos = payload.indexOf(',', pos+1)) != -1){
+            const auto tmp = payload.substring(pos_old, pos);
+            strcpy(values[ctr++], tmp.substring(tmp.indexOf(':') == -1 ? 0 : tmp.indexOf(':') + 1).c_str());
+            //Serial.println(tmp.substring(tmp.indexOf(':') == -1 ? 0 : tmp.indexOf(':') + 1));
+
+            pos_old = pos;
+            //delay(1000);
+        }
+        if(payload.length() - pos_old) {
+            const auto tmp = payload.substring(pos_old, payload.length() - 1);
+            strcpy(values[ctr++], tmp.substring(tmp.indexOf(':') == -1 ? 0 : tmp.indexOf(':') + 1).c_str());
+            //Serial.println(tmp.substring(tmp.indexOf(':') == -1 ? 0 : tmp.indexOf(':') + 1));
+        }
+
+        const char* ptr = values[0];
+
+        Serial.println(((ptr[1] - 48) * 10 + (ptr[2] - 48)) * 3600 +  ((ptr[4] - 48) * 10 + (ptr[5] - 48))* 60);
+        //for (const auto& val: values)
+        //    Serial.println(val);
         server.send(200);
     });
 
@@ -122,63 +157,174 @@ void setup() {
     //    Serial.println(data); //Выводим информацию в монитор порта
 }
 
-
-//Вариант не работает с режимом точки доступа
-static PT_THREAD(web_server) {
-    PT_BEGIN(pt);
-    while (true) {
-        //PT_YIELD_UNTIL(pt, WiFi.status() != WL_CONNECTED);
-        server.handleClient();
-        PT_YIELD(pt);
-    }
-
-    PT_END(pt);
-
+void byte_write(uint16_t address, uint8_t data){
+    Wire.beginTransmission(EEPROM_ADDR);
+    Wire.write(address >> 8);
+    Wire.write(address & 0xFF);
+    Wire.write(data);
+    Wire.endTransmission();
+    delay(10);
 }
+
+void page_write(uint16_t address, const uint8_t* data, const size_t length){
+    for (size_t i = 0; i < length / PAGE_SIZE; ++i, address += PAGE_SIZE) {
+        Wire.beginTransmission(EEPROM_ADDR);
+        Wire.write(address >> 8);
+        Wire.write(address & 0xFF);
+        Wire.write(data + i * PAGE_SIZE, PAGE_SIZE);
+        Wire.endTransmission();
+        delay(10);
+        Serial.print("Write page #");
+        Serial.println(i);
+    }
+    if((length % PAGE_SIZE) == 0)
+        return;
+    Serial.print("Write rest [");
+    Serial.print(length % PAGE_SIZE);
+    Serial.println("]");
+    Wire.beginTransmission(EEPROM_ADDR);
+    Wire.write(address >> 8);
+    Wire.write(address & 0xFF);
+    Wire.write(data + (length / PAGE_SIZE) * PAGE_SIZE, length % PAGE_SIZE);
+    Wire.endTransmission();
+    delay(10);
+
+
+//    Wire.beginTransmission(EEPROM_ADDR);
+//    Wire.write(address >> 8);
+//    Wire.write(address & 0xFF);
+//    //Wire.write(reinterpret_cast<const uint8_t*>(data), length);
+//    for (int i = 0; i < length; i++) {
+//        Wire.write(data[i]);
+//    }
+//
+//
+//    Wire.endTransmission();
+//    delay(10);
+}
+
+uint16_t address_read(){
+    uint16_t out = 0;
+    Serial.println(Wire.requestFrom(EEPROM_ADDR, sizeof(out)));
+    Wire.readBytes(reinterpret_cast<char *>(&out), sizeof(out));
+    return out;
+}
+
+void random_read(uint16_t address, uint8_t* buffer, int length){
+    Wire.beginTransmission(EEPROM_ADDR);
+    Wire.write(address >> 8);
+    Wire.write(address & 0xFF);
+    Wire.endTransmission();
+
+    Serial.println(Wire.requestFrom(EEPROM_ADDR, length));
+//    Wire.readBytes(buffer, length);
+    for (int i = 0; i < length; i++) {
+        if (Wire.available()) {
+            buffer[i] = Wire.read(); // Читаем каждый байт данных
+        } else {
+            buffer[i] = 0xFF; // Если чтение не удалось, возвращаем 0xFF
+        }
+    }
+}
+
 
 static PT_THREAD(network_monitor) {
     static async_wait delay(0);
     PT_BEGIN(pt);
 
-    while (true) {
-        PT_YIELD_UNTIL(pt, !delay);
+                while (true) {
+                    PT_YIELD_UNTIL(pt, !delay);
 
-        if (WiFi.status() == WL_CONNECTED) {
-            Serial.print("Connected[");
-            Serial.print(SSID);
-            Serial.print("]");
-            logMessage("IP адрес: " + WiFi.localIP().toString());
-            PT_EXIT(pt);
-        }
+                    if (WiFi.status() == WL_CONNECTED) {
+                        Serial.print("Connected[");
+                        Serial.print(SSID);
+                        Serial.print("]");
+                        logMessage("IP адрес: " + WiFi.localIP().toString());
+                        PT_EXIT(pt);
+                    }
 
-        if (tries_ctr >= MAX_WIFI_TRIES) {
-            Serial.println("Too many tries. Timeout");
-            tries_ctr = 0;
-            WiFi.disconnect();
-            delay(5 * 60 * 1000);
-        }
-        else {
-            if (tries_ctr++ == 0)
-                WiFi.begin(SSID, PASSWORD);
-            Serial.print("Connecting to WIFI [");
-            Serial.print(SSID);
-            Serial.print("] #");
-            Serial.println(tries_ctr);
-            delay(1 * 1000);
-        }
-        PT_YIELD(pt);
-    }
+                    if (tries_ctr >= MAX_WIFI_TRIES) {
+                        Serial.println("Too many tries. Timeout");
+                        tries_ctr = 0;
+                        WiFi.disconnect();
+                        delay(5 * 60 * 1000);
+                    }
+                    else {
+                        if (tries_ctr++ == 0)
+                            WiFi.begin(SSID, PASSWORD);
+                        Serial.print("Connecting to WIFI [");
+                        Serial.print(SSID);
+                        Serial.print("] #");
+                        Serial.println(tries_ctr);
+                        delay(1 * 1000);
+                    }
+                    PT_YIELD(pt);
+                }
 
     PT_END(pt);
 }
 
+//TODO: Вариант не работает с режимом точки доступа
+//TODO: Не ждет подключения к ТД или поднятия хотспоста
+//TODO: Остановка сервера при простое переподключения
+static PT_THREAD(web_server) {
+    PT_BEGIN(pt);
+                while (true) {
+                    server.handleClient();
+                    PT_YIELD(pt);
+                }
+    PT_END(pt);
+}
 
 
 void loop() {
-//    Wire.beginTransmission(DS1307_ADDR);
+    PT_SCHEDULE(network_monitor, nm_context);
+    PT_SCHEDULE(web_server, web_server_context);
+//    byte error, address;
+//    int nDevices;
+//
+//    Serial.println("Scanning...");
+//
+//    nDevices = 0;
+//    for(address = 1; address < 127; address++ )
+//    {
+//        // The i2c_scanner uses the return value of
+//        // the Write.endTransmisstion to see if
+//        // a device did acknowledge to the address.
+//        Wire.beginTransmission(address);
+//        error = Wire.endTransmission();
+//
+//        if (error == 0)
+//        {
+//            Serial.print("I2C device found at address 0x");
+//            if (address<16)
+//                Serial.print("0");
+//            Serial.print(address,HEX);
+//            Serial.println("  !");
+//
+//            nDevices++;
+//        }
+//        else if (error==4)
+//        {
+//            Serial.print("Unknown error at address 0x");
+//            if (address<16)
+//                Serial.print("0");
+//            Serial.println(address,HEX);
+//        }
+//    }
+//    if (nDevices == 0)
+//        Serial.println("No I2C devices found\n");
+//    else
+//        Serial.println("done\n");
+//
+//
+//
+//
+//
+//    Wire.beginTransmission(RTC_ADDR);
 //    Wire.write(0);  // Начинаем с регистра 0
 //    Wire.endTransmission();
-//    Serial.println(Wire.requestFrom(DS1307_ADDR, 8));
+//    Serial.println(Wire.requestFrom(RTC_ADDR, 8));
 //    while(Wire.available()){
 //        Serial.print("Got DS byte: ");
 //        uint8_t ret = Wire.read();
@@ -189,7 +335,28 @@ void loop() {
 //
 //        Serial.println("}");
 //    }
-//    delay(1000);           // wait 5 seconds for next scan
-    PT_SCHEDULE(network_monitor, nm_context);
-    PT_SCHEDULE(web_server, web_server_context);
+////    delay(1000);           // wait 5 seconds for next scan
+//Serial.println(address_read(), HEX);
+//    const uint8_t dataw[35] = {0x45, 0x33, 0xfc, 0xee, 0x03};
+//   page_write(0x0005, dataw, 35);
+//
+//
+//    const int numBytes = 15; // Количество байт для чтения
+//    byte data[numBytes]; // Массив для хранения считанных данных
+//
+//    // Пример чтения 5 байт с адреса 0x0005
+//    random_read(0x0000, data, numBytes);
+//
+//    Serial.print("Прочитанные данные: ");
+//    for (int i = 0; i < numBytes; i++) {
+//        Serial.print(data[i], HEX); // Выводим данные в шестнадцатеричном формате
+//        Serial.print(" ");
+//    }
+//    Serial.println();
+//
+//    delay(1000); // Задержка между циклами
+
+
 }
+
+
